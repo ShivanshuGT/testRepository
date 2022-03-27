af4e48cf4be9fd2a009d0fdcef57dd9e9500dd84
ShivanshuGT <shivanshumdnr@gmail.com>
2022-03-27 07:11:41 +0530

    Initial commit

---
+++ bikeTest/src/com/biketest/BikeMain.java
@@ -0,0 +1,11 @@
+package com.biketest;
+
+public class BikeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bike running successfully");
+
+	}
+
+}
